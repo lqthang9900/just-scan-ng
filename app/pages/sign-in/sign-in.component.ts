992cbd4290aad918db823bad55817d063312f8a9
Le Thang <lqthang9900@gmail.com>
2017-09-25 16:25:57 +0700

    add sign in

--- app/pages/sign-in/sign-in.component.ts
+++ app/pages/sign-in/sign-in.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { verifyInput, Utils, Config } from '../../shared';
+import { isAndroid } from 'platform';
+import { topmost } from 'ui/frame';
+
 
 @Component({
 	selector: 'sign-in',
@@ -8,8 +12,81 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class SignInComponent implements OnInit {
+	private signInConfig: any;
+	constructor() {
+		this.signInConfig = {
+			userName: {
+				label: "Tài khoản",
+				type: "id",
+				id: "userName",
+				errors: {
+					required: {
+						error: false,
+						message: Config.ERROR_MESSAGE.USERNAME_REQUIRED
+					},
+					format: {
+						error: false,
+						message: Config.ERROR_MESSAGE.USERNAME_FORMAT
+					}
+				},
+				messageError: " ",
+				error: false,
+				placeHolder: "Tài khoản",
+				value: ""
+			}, 
+			password: {
+				label: "Mật khẩu",
+				type: "password",
+				id: "password",
+				errors: {
+					required: {
+						error: false,
+						message: Config.ERROR_MESSAGE.PASSWORD_REQUIRED
+					},
+					format: {
+						error: false,
+						message: Config.ERROR_MESSAGE.PASSWORD_FORMAT
+					}
+				},
+				messageError: " ",
+				error: false,
+				placeHolder: "Mật khẩu",
+				value: ""
+			},
+		}
+		if (isAndroid) {
 
-	constructor() { }
+		}
+		else {
+			topmost().ios.controller.navigationBar.barStyle = 1;
+		}
+	}
 
-	ngOnInit() { }
+	ngOnInit() {
+
+	}
+
+	checkInput(args) {
+		verifyInput(args.object.id, this, this.signInConfig, 'signInConfig');
+	}
+
+	signIn(args) {
+		var validated = true;
+		for (let i in this.signInConfig) {
+			if (!verifyInput(this.signInConfig[i].id, this, this.signInConfig, 'signInConfig')) {
+				validated = false;
+				Utils.toastAlert(this.signInConfig[i].messageError);
+				break;
+			}
+		}
+		if (validated) {
+			console.log('id: ' + this.signInConfig.userName.value);
+			console.log('password: ' + this.signInConfig.password.value);
+		}
+		
+	}
+
+	signUp(args) {
+		Utils.navigate('pages/sign-up/sign-up');
+	}
 }
--- app/pages/sign-in/sign-in.module.ts
+++ app/pages/sign-in/sign-in.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from "@angular/core";
 
 import { signInRouting } from "./sign-in.routing";
 import { SignInComponent } from "./sign-in.component";
+import { TNSFontIconModule } from "nativescript-ngx-fonticon";
 
 @NgModule ({
     imports: [
         NativeScriptModule,
         NativeScriptFormsModule,
         signInRouting,
-        // ForgotPasswordModule
+        TNSFontIconModule.forRoot({
+            'fa': 'fonts/font-awesome.css'
+        }),
     ],
     declarations: [
         SignInComponent
